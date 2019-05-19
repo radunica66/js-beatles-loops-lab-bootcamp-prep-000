019f2028dcdccefec7b50fe7b6fbe57acc3522ed
radunica66 <mislav.cotic@gmail.com>
2019-05-19 06:51:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 // add solution here
-let emptyArr = []
+let emptyArray = []
 function theBeatlesPlay(arrayMusicians, arrayInstruments){
   for(let i = 0; i < arrayMusicians.length; i++){
       emptyArr.push(arrayMusicians[i] + " plays "+ arrayInstruments[i])
   }
 
-  return(emptyArr)
+  return(emptyArray)
 }
