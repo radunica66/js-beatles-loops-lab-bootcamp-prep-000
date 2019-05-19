644c182dc019d5e08dc465001d76132dadd77e5a
radunica66 <mislav.cotic@gmail.com>
2019-05-19 06:51:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 let emptyArray = []
 function theBeatlesPlay(arrayMusicians, arrayInstruments){
   for(let i = 0; i < arrayMusicians.length; i++){
-      emptyArr.push(arrayMusicians[i] + " plays "+ arrayInstruments[i])
+      emptyArray.push(arrayMusicians[i] + " plays "+ arrayInstruments[i])
   }
 
   return(emptyArray)
